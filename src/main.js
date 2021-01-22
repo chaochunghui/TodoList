d322ae9ecae6ad635c36572b0de4952e74cacee5
chaoahunghui <qwert1997603@gmail.com>
2021-01-22 22:36:52 +0800

    todolist

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { sync } from 'vuex-router-sync'
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+sync(store, router)
 
+Vue.config.productionTip = false
+    // Register a global custom directive called `v-focus`
+Vue.directive('focus', {
+    // When the bound element is inserted into the DOM...
+    inserted: function(el) {
+        // Focus the element
+        el.focus()
+    }
+})
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+        path: '/all',
+        name: 'all',
+
+    },
+    {
+        path: '/active',
+        name: 'active',
+
+    },
+    {
+        path: '/complete',
+        name: 'complete',
+
+    },
+    {
+        path: '*',
+        redirect: 'all',
+
+    },
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 export default router
